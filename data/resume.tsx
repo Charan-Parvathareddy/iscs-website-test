51aebeb8c354fb987cb8e9eb338547b563d3ff1d
Charan-Parvathareddy <parvathareddy.charan@gmail.com>
2024-11-06 16:50:04 +0530

    152

--- data/resume.tsx
+++ data/resume.tsx
@@ -27,9 +27,8 @@ export const DATA = {
           icon: <Icons.globe className="size-3" />,
         },
       ],
-      image: "",
-      video:
-        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/chat-collect.mp4",
+      image: "@/assets/G-Nayana/diabetic-retinopathy.jpg",
+      video: "",
     },
     {
       title: "Integrated HRMS",
