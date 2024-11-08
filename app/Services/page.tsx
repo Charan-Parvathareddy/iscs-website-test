d89bb782aedf108b7fb19e2487dbe4782fd8a839
Charan-Parvathareddy <parvathareddy.charan@gmail.com>
2024-11-08 12:23:17 +0530

    214

--- app/Services/page.tsx
+++ app/Services/page.tsx
@@ -4,7 +4,7 @@ import Img2 from "@/public/assets/ai/ai.png";
 import Img3 from "@/public/assets/salesforce/salesforce.webp";
 import Img4 from "@/public/assets/Oracle/oracle-consulting.png";
 import Img5 from "@/public/assets/staffing/it-consulting.png";
-import Img6 from "@/public/assets/Oracle/oracle-peoplesoft-solution.png";
+import Img6 from "@/public/assets/Oracle/oracle-peoplesoft-min.png";
 import Img7 from "@/public/assets/cloud-services.jpg";
 
 import Banner1 from "@/components/Products/Banner1";
