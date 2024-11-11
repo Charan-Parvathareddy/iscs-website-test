96b72248b7be72f0136fdf8580995f8052b97a6c
Charan-Parvathareddy <parvathareddy.charan@gmail.com>
2024-11-11 19:08:02 +0530

    229

--- components/Navbar/Navigation.tsx
+++ components/Navbar/Navigation.tsx
@@ -31,6 +31,15 @@ interface NavItemProps {
   isResourceItem?: boolean
 }
 
+const colors = {
+  primary: '#0066CC',    // Blue
+  secondary: '#FF3366',  // Red
+  accent: '#FFB800',     // Yellow
+  green: '#009944',      // Green from ISCS logo
+  text: '#333333'
+}
+
+
 const NavItem: React.FC<NavItemProps> = ({ 
   name, 
   href, 
@@ -550,7 +559,12 @@ const Navigation: React.FC = () => {
               Log In
             </Button>
             </Link>
-            <Button variant="default" onClick={handleContactClick}>
+            <Button variant="default" size="lg" 
+                className="gap-2" 
+                style={{
+                  backgroundColor: colors.primary,
+                  color: 'white'
+                }} onClick={handleContactClick}>
              Contact Us
             </Button>
            
